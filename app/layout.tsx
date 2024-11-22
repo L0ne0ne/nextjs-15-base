b22295060fbbcdd62aa008210c83f61c836b6614
Loneone <106025305+L0ne0ne@users.noreply.github.com>
2024-11-22 12:41:46 +0330

    made it better

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,7 +5,7 @@ import localFont from 'next/font/local';
 
 import { ThemeProvider } from 'next-themes';
 
-import '@/app/globals.css';
+import './globals.css';
 
 const geistSans = localFont({
     src: './fonts/GeistVF.woff',
